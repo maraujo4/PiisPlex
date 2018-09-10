3b3dcab43291d7dcbcb8f8345202af47355f3990
Monica Araujo <priscila_monica78@hotmail.com>
2018-09-10 18:44:03 -0400

    Initial commit for piisPlex, some of the images may be deleted later

---
+++ PiisPlex/PiisPlex/PiisPlex/Pages/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PiisPlex.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PiisPlex.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            LoginViewModel loginView = new LoginViewModel();
+            this.BindingContext = loginView;
+            loginView.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+            InitializeComponent();
+
+            Email.Completed += (object sender, EventArgs e) => Password.Focus();
+            Password.Completed += (object sender, EventArgs e) => loginView.SubmitCommand.Execute(null);
+        }        
+    }
+}
---
+++ PiisPlex/PiisPlex/PiisPlex/ViewModels/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PiisPlex.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public Action DisplayInvalidLoginPrompt;
+        private string m_email;
+        public string Email
+        {
+            get { return m_email; }
+            set { m_email = value; PropertyChanged(this, new PropertyChangedEventArgs("Email")); }
+        }
+
+        private string m_password;
+        public string Password
+        {
+            get { return m_password; }
+            set { m_password = value; PropertyChanged(this, new PropertyChangedEventArgs("Password")); }
+        }
+
+        public ICommand SubmitCommand { protected set; get; }
+        public LoginViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+
+        public void OnSubmit()
+        {
+            if (Email != "abc" || Password != "abc")
+                DisplayInvalidLoginPrompt();
+        }
+    }
+}
